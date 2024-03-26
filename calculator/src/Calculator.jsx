56f0a15ddb8a7a074b294936c90a6df7c136c8a4
Dron Kishor <dronkishor2002@gmail.com>
2024-03-26 14:15:39 +0530

    Change name

--- calculator/src/Calculator.jsx
+++ calculator/src/Calculator.jsx
@@ -40,7 +40,7 @@ export default function Calculator() {
       <button>Dron</button>
 
       <button onClick={aparm} value="2">
-        EK aur button
+        EK aur btn
       </button>
       <button onClick={aparm} value="2">
         Dron aur button
